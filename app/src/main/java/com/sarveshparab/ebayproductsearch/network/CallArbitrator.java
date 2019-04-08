0dd5187c5f171cbb7eb7f063c9e70f6579a9dc16
Sarvesh Parab <sparab@usc.edu>
2019-04-07 20:05:32 -0700

    volley call arbitrator for any url call made

---
+++ app/src/main/java/com/sarveshparab/ebayproductsearch/network/CallArbitrator.java
@@ -0,0 +1,60 @@
+package com.sarveshparab.ebayproductsearch.network;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.sarveshparab.ebayproductsearch.utility.StrUtil;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CallArbitrator {
+
+    public static void makeRequest(final String url, final NetworkCallBack callback, Context ctx, final Map<String, String> params) {
+        JSONRequest rq = new JSONRequest(Request.Method.GET, url, params, new Response.Listener() {
+            @Override
+            public void onResponse(Object response) {
+                Log.v(StrUtil.LOG_TAG+"|NetworkSuccess", response.toString());
+                try {
+                    JSONObject jResponse = (JSONObject)response;
+                    callback.onSuccess(jResponse);
+                } catch (Exception e) {
+                    Log.v(StrUtil.LOG_TAG+"|NetworkSuccess|ExceptionRaised", e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v(StrUtil.LOG_TAG+"|NetworkError", error.toString());
+                try {
+                    callback.onError(error.toString());
+                } catch (Exception e) {
+                    Log.v(StrUtil.LOG_TAG+"|NetworkError|ExceptionRaised", e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return new HashMap<>();
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                return params;
+            }
+
+        };
+
+        VolleyHandler.getInstance(ctx).addToRequestQueue(rq);
+    }
+
+}
