71a40322c1728d662c3527019cb1c37bb810a563
Sarvesh Parab <sparab@usc.edu>
2019-04-07 20:15:32 -0700

    string utility class to hold global static strings

---
+++ app/src/main/java/com/sarveshparab/ebayproductsearch/utility/StrUtil.java
@@ -0,0 +1,15 @@
+package com.sarveshparab.ebayproductsearch.utility;
+
+public class StrUtil {
+
+    public static final String LOG_TAG = "EPS";
+
+    public static final String IP_API_URL = "http://ip-api.com/json";
+    public static final String IP_API_ZIP_KEY = "zip";
+
+
+    public static final String JSON_RESPONSE_STATUS_KEY = "ResponseStatus";
+    public static final String JSON_RESPONSE_MESSAGE_KEY = "ResponseMessage";
+    public static final String JSON_ERROR_RESPONSE = "Error";
+    public static final String JSON_SUCCESS_RESPONSE = "Success";
+}
