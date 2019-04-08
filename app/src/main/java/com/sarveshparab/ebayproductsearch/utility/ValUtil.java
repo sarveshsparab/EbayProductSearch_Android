33c3b8a5cda94972da4f867439474bb4d0be9265
Sarvesh Parab <sparab@usc.edu>
2019-04-08 15:39:36 -0700

    auto complete zip values added

---
+++ app/src/main/java/com/sarveshparab/ebayproductsearch/utility/ValUtil.java
@@ -0,0 +1,9 @@
+package com.sarveshparab.ebayproductsearch.utility;
+
+public class ValUtil {
+
+    public static final int INITIATE_ZIP_AUTOCOMPLETE = 100;
+    public static final long ZIP_AUTOCOMPLETE_DELAY = 300;
+
+    public static final int ZIP_THRESHOLD = 2;
+}
