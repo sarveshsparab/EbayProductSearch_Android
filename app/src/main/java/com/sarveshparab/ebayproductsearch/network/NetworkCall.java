6a57b4bf0fa661768b153cf115eb0c66f5cc5976
Sarvesh Parab <sparab@usc.edu>
2019-04-08 16:24:22 -0700

    moved currzipcode call to fragment to avoid null handling

--- app/src/main/java/com/sarveshparab/ebayproductsearch/network/NetworkCall.java
+++ app/src/main/java/com/sarveshparab/ebayproductsearch/network/NetworkCall.java
@@ -15,31 +15,6 @@ import java.util.List;
 
 public class NetworkCall {
 
-    public static JSONObject currZipCode;
-
-    public static void fetchCurrLocation(Context actCtx) {
-        CallArbitrator.makeRequest(StrUtil.IP_API_URL, new NetworkCallBack() {
-            @Override
-            public void onSuccess(JSONObject result) throws JSONException {
-                JSONObject jsonObject = new JSONObject()
-                        .put(StrUtil.JSON_RESPONSE_STATUS_KEY, StrUtil.JSON_SUCCESS_RESPONSE)
-                        .put(StrUtil.JSON_RESPONSE_MESSAGE_KEY, result.getString(StrUtil.IP_API_ZIP_KEY));
-
-                currZipCode = jsonObject;
-            }
-
-            @Override
-            public void onError(String result) throws Exception {
-                Log.v(StrUtil.LOG_TAG+"|IP-API_Error", "Call failed with error : "+result);
-                JSONObject jsonObject = new JSONObject()
-                        .put(StrUtil.JSON_RESPONSE_STATUS_KEY, StrUtil.JSON_ERROR_RESPONSE)
-                        .put(StrUtil.JSON_RESPONSE_MESSAGE_KEY, "IP-API call failed");
-
-                currZipCode = jsonObject;
-            }
-        }, actCtx, null);
-    }
-
     public static void fetchAutoCompleteZip(String text, final ZipAutoAdapter zipAutoAdapter,
                                             Context actCtx) {
         CallArbitrator.makeRequest(StrUtil.NODE_BASE + StrUtil.NODE_ZIP_AUTO + text,
